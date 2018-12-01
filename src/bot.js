3f59bf022a08b0bd32578ff4588c41d7d6baef7b
BryanBattershill <bryan.battershill@gmail.com>
2018-12-01 10:18:30 -0500

    Initial test bot commit

---
+++ src/bot.js
@@ -0,0 +1,31 @@
+var Discord = require('discord.io');
+var auth = require('./auth.json');
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
